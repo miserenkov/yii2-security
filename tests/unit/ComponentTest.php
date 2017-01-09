0daae58c5ad9e397c25fe2c366fa9b87f61bb10d
Misha Serenkov <mi.serenkov@gmail.com>
2017-01-09 17:41:05 +0200

    base tests

--- tests/unit/ComponentTest.php
+++ tests/unit/ComponentTest.php
@@ -7,7 +7,20 @@
  * Date: 09.01.2017 15:51
  */
 
+/**
+ * Class ComponentTest
+ * @property \Codeception\Module\Yii2 $tester
+ */
 class ComponentTest extends Codeception\Test\Unit
 {
+    public function testOpenSSLExtension()
+    {
+        $this->assertTrue(extension_loaded('openssl'));
+    }
 
+    public function testValidSecurityComponent()
+    {
+        $component = Yii::$app->get('security');
+        $this->assertInstanceOf('miserenkov\security\Security', $component);
+    }
 }
